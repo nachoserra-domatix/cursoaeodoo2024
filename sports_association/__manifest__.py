456c448500e1d0d3ca90a6899f28af9c527e8a99
Daniel <douglas.sulbaran.bakata@gmail.com>
2024-02-26 17:22:49 +0000

    [ADD] sport association module

---
+++ sports_association/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sports Association",
+    "summary": "Sports Association",
+    "version": "17.0",
+    "category": "Uncategorized",
+    "author": "<AUTHOR(S)>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sport_issue.xml",
+    ],
+}
