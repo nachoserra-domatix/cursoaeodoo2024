0d6668d6c5ca02558528f5b3c059e80ae3467cea
potxolate <potxolate@hotmail.com>
2024-02-26 15:27:48 +0000

    creacion modelo sport issue

---
+++ sports_association/models/sport_issue.py
@@ -0,0 +1,30 @@
+# Copyright 2024 potxolate
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+class SportIssue(models.Model):
+    _name = 'sport.issue'
+    _description = "sport issue"
+
+    name = fields.Char(string="name")
+    description = fields.Text(readonly=True, states={"draft": [("readonly", False)]})
+    date = fields.Datetime(
+        readonly=True,
+        required=True,
+        string="Date",
+        default=fields.Datetime.now,        
+        help="Issue date.",
+    )
+    assitance = fields.Boolean(string="Assistance")
+    state = fields.Selection(
+        selection=[
+            ("draft", "Draft"),
+            ("open", "Open"),
+            ("done", "Done"),
+            ("expired", "Expired"),
+        ],
+        string="State",
+        default="draft",
+    )
+    
