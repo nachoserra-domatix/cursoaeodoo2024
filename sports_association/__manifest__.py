2401d85f72e5d47d311722ba16fc2c9f36b033e3
Carlos Borrás <carlos.borras@playademazagon.com>
2024-02-26 17:24:16 +0000

    New Module Sport Issue

---
+++ sports_association/__manifest__.py
@@ -0,0 +1,48 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sports Association",
+    "summary": "Module summary",
+    "version": "17.0.1.0.0",
+    # see https://odoo-community.org/page/development-status
+    #"development_status": "Alpha|Beta|Production/Stable|Mature",
+    "category": "Sports",
+    #"website": "https://github.com/OCA/<repo>" or "https://github.com/OCA/<repo>/tree/<branch>/<addon>",
+    #"author": "<AUTHOR(S)>, Odoo Community Association (OCA)",
+    # see https://odoo-community.org/page/maintainer-role for a description of the maintainer role and responsibilities
+    #"maintainers": ["your-github-login"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    #"preloadable": True,
+    #"pre_init_hook": "pre_init_hook",
+    #"post_init_hook": "post_init_hook",
+    #"post_load": "post_load",
+    #"uninstall_hook": "uninstall_hook",
+    # "external_dependencies": {
+    #     "python": [],
+    #     "bin": [],
+    # },
+    "depends": [
+        "base",
+    ],
+    # this feature is only present for 11.0+
+    # "excludes": [
+    #     "module_name",
+    # ],
+    "data": [
+        # security/some_model_security.xml",
+        "security/ir.model.access.csv",
+        # "templates/assets.xml",
+        # "views/report_name.xml",
+        "views/sport_issue.xml",
+        # "wizards/wizard_model_view.xml",
+    ],
+    "demo": [
+        "demo/assets.xml",
+        "demo/res_partner_demo.xml",
+    ],
+    # "qweb": [
+    #     "static/src/xml/module_name.xml",
+    # ]
+}
---
+++ sports_association/models/sport_issue.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+class SportIssue(models.Model):
+    _name ='sport.issue'
+    _description = 'Description'
+    
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    date = fields.Date(string='Date')
+    assistance = fields.Boolean(string='Assistance',help='Show if the issue has assistance')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('open', 'Open'),
+        ('done', 'Done'),],
+        string='State',
+        default='draft',)
+    
+
+
