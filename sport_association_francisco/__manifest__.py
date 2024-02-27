bbf4e303f632189415a87daf2667b27f88c6198a
FranRM <chiscazo.paco@gmail.com>
2024-02-27 14:09:24 +0000

    [NEW] Sport Association Module

---
+++ sport_association_francisco/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sports Association Francisco",
+    "summary": "A sport association module",
+    "version": "17.0.1.0.0",
+    "category": "Sport",
+    "author": "<FranRM(S)>",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "base",
+    ],
+    "data":[
+        "security/ir.model.access.csv",
+        "views/sport_issue.xml"
+    ]
+}
