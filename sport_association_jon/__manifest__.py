ab2da4800fe9f572718023237a75caf46f50d9f5
JonGonzalezD5 <jongonzalez@digital5.es>
2024-02-27 08:27:06 +0000

    Module created and README updated

---
+++ sport_association_jon/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sport Associations",
+    "summary": "Manage sports association members, teams and events",
+    "version": "17.0.1.0.0",
+    "category": "Sport module",
+    "author": "Jon",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data":[
+        "views/sport_issue.xml",
+        "security/ir.model.access.csv"
+    ],
+}
