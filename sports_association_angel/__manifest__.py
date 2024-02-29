47e1d543b5bac80a5487933bee9ba691c6546e62
angelmartserr <angelmartserr@gmail.com>
2024-02-29 14:47:07 +0000

    Nuevos cambios

--- sports_association_angel/__manifest__.py
+++ sports_association_angel/__manifest__.py
@@ -1,9 +1,10 @@
-# Copyright <YEAR(S)> <AUTHOR(S)>
+# Copyright <2024> <Angel Martinez>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sports Association",
-    "summary": "Manage sports",
+    "summary": "Manage sports association members, teams, events",
     "version": "17.0.1.0.0",
+    "category": "Sports",
     "author": "Angel Martinez",
     "license": "AGPL-3",
     "application": False,
@@ -11,8 +12,13 @@
     "depends": [
         "base",
     ],
-    # this feature is only present for 11.0+
-    "data": [
-        "views/sport_issue.xml",
-    ],
+
+    "data": ["data/sport_license_data.xml",
+             "security/groups.xml",
+             "security/ir.model.access.csv",
+             "views/sport_issue.xml",
+             "views/sport_issue_tag_views.xml",
+             "views/sport_clinic_views.xml",
+             "views/sport_issue_views.xml",
+             ],
 }
--- sports_association_angel/models/__init__.py
+++ sports_association_angel/models/__init__.py
@@ -1,1 +1,4 @@
 from . import sport_issue
+from . import sport_license
+from . import sport_clinic
+from . import sport_issue_tag
