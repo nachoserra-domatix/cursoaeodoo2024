c0c1b89575423d9148312678eded3f5d4f6afdd1
Sergio Torres <storres@ecomvalue21.com>
2024-03-05 17:28:23 +0000

    [MOD] models issue, player, team, vistas issue, player, team

--- sports_association_sergio/models/sport_issue.py
+++ sports_association_sergio/models/sport_issue.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api, Command   
 
 class SportIssue(models.Model):
     _name =  'sport.issue'
@@ -25,7 +25,40 @@ class SportIssue(models.Model):
     tags_ids = fields.Many2many('sport.issue.tag', string='Tags')
     color = fields.Integer(string="Color",default=0)
     cost =fields.Float(string="Cost", default=0.0)
-                                 
+
+    assigned = fields.Boolean(name="Assigned", compute="_compute_assigned", inverse="_inverse_assigned", search="_search_assigned",store=True)
+    user_phone = fields.Char(string='User Phone', related='user_id.partner_id.phone', store=True)  
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            # import pdb;pdb.set_trace()
+            record.assigned = bool(record.user_id)
+
+    def _inverse_assigned(self):
+        for record in self:
+            if not record.assigned:
+                record.user_id = False 
+            else:
+                record.user_id = self.env.user
+
+    def _search_assigned(self, operator, value):
+        if operator == '=':
+            return [('user_id', operator, value)]
+        else:
+            return []
+
+    def action_find_tags(self):
+        for record in self:
+            if record.name:
+                tag_ids = self.env['sport.issue.tag'].search([('name','ilike',record.name)])
+                if tag_ids:
+                    record.tags_ids = [(6, 0, tag_ids.ids)]
+                else:
+                    record.tags_ids = [Command.create({'name': record.name})]
+                    record.tags_ids = [(0, 0, {'name':record.name})]
+
+
     def action_open(self):
         # import pdb;pdb.set_trace()
 
--- sports_association_sergio/models/sport_team.py
+++ sports_association_sergio/models/sport_team.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, Command
 
 class SportTeam(models.Model):
     _name = 'sport.team'
@@ -8,10 +8,25 @@ class SportTeam(models.Model):
     logo = fields.Binary(string='Logo')
     sport_id = fields.Many2one('sport.sport', string='Sport')
     player_ids = fields.One2many('sport.player', 'team_id', string="Players")
+    player_count = fields.Integer(string='Player Count', compute='_compute_player_count')
 
+    def _compute_player_count(self):
+        for record in self:
+            record.player_count = len(record.player_ids)
+    
     def action_mark_starters(self):
         for player in self.player_ids:
             player.action_markself_starter()
     
     def action_unmark_starters(self):
         self.player_ids.write({'starter': False})
+
+    def action_add_players(self):
+        for record in self:
+            players = self.env['sport.player'].search([('team_id', '=', False), ('age','<',30)])
+
+            # for player in players:
+            #     player.team_id = self.id
+
+            players |= record.player_ids
+            record.player_ids = [Command.set(players.ids)]
