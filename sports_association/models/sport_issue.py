605c5c38e9508403fbeb42b16276a5cf1967391f
potxolate <potxolate@hotmail.com>
2024-03-07 08:08:39 +0000

    [ADD] task5

--- sports_association/models/sport_issue.py
+++ sports_association/models/sport_issue.py
@@ -1,7 +1,7 @@
 # Copyright 2024 potxolate
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 SPORT_ISSUE_STATE = [
     ('draft', "Draft"),
@@ -45,6 +45,25 @@ class SportIssue(models.Model):
         string="Tag")
     cost = fields.Integer(string="Cost")
     color = fields.Integer(string="Color")
+    assigned = fields.Boolean('Assigned', compute='_compute_assigned', inverse='_inverse_assigned', store=True)
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = bool(record.user_id)
+    
+    def _inverse_assigned(self):
+        for record in self:
+            if not record.assigned:
+                record.user_id = False
+            else:
+                record.user_id = self.env.user
+
+    def _search_assigned(self, operator, value):
+        if operator == '=':
+            return [('user_id', operator, value)]
+        else:
+            return []
 
     #=== METHODS ===#
 
--- sports_association/models/sport_player.py
+++ sports_association/models/sport_player.py
@@ -1,19 +1,29 @@
 # Copyright 2024 potxolate
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 class SportPlayer(models.Model):
     _name = 'sport.player'
     _description = 'Player'
 
     name = fields.Char(string='Player Name', required=True)
-    age = fields.Integer(string='Age')
+    birthdate = fields.Date('Birthdate')
+    age = fields.Integer('Age', compute='_compute_age', store=True)
     position = fields.Char(string='Position')
     team_id = fields.Many2one('sport.team', string='Team')
     is_starter = fields.Boolean(string='Starter')
     photo = fields.Image(string="Photo")
+    sport_name = fields.Char('Sport', related='team_id.sport_id.name')
 
+    @api.depends('birthdate')
+    def _compute_age(self):
+        for record in self:
+            if record.birthdate:
+                record.age = (fields.Date.today() - record.birthdate).days / 365
+            else:
+                record.age = 0
+    
     #=== METHODS ===#
 
     def action_starter (self):
