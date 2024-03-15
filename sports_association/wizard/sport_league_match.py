b51d75400fdeb351f412b1f8c948481d116fac41
potxolate <potxolate@hotmail.com>
2024-03-15 16:47:06 +0000

    [ADD] last tasks

--- sports_association/wizard/sport_league_match.py
+++ sports_association/wizard/sport_league_match.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 class SportLeagueMatch(models.TransientModel):
     _name = 'sport.league.match'
     _description = 'Create Matchs'
 
+    league_id = fields.Many2one('sport.league', string='League')
     team_ids = fields.Many2many('sport.team', string='Teams')
     sport_id = fields.Many2one('sport.sport', string='Sport')
     date = fields.Datetime(string='Match date')
+    allowed_team_ids = fields.Many2many('sport.team', relation='sport_match_team_rel', string='Allowed Teams', compute='_compute_allowed_team_ids', store=True)
+
+    @api.depends('league_id')
+    def _compute_allowed_team_ids(self):
+        for record in self:
+            record.allowed_team_ids = self.league_id.league_line_ids.team_id.ids
 
     def create_matchs(self):
         vals = {
