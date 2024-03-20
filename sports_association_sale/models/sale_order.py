670e45ddbed854b1370297da73aff0c18e335419
potxolate <potxolate@hotmail.com>
2024-03-20 17:10:05 +0000

    [ADD] task 12

--- sports_association_sale/models/sale_order.py
+++ sports_association_sale/models/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -9,3 +9,17 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     sport_ticket_ids = fields.One2many('sport.ticket', 'sale_order_id', string='Sport Tickets')
+
+    def action_cancel(self):
+        # res= super(SaleOrder,self).action_cancel()
+        res = super().action_cancel()
+        self.sport_ticket_ids.unlink()
+        return res
+
+    def create_sport_ticket(self):
+        vals = {
+            'name': self.name,
+            'partner_id': self.partner_id.id,
+            'sale_order_id': self.id
+        }
+        self.env['sport.ticket'].create(vals)
