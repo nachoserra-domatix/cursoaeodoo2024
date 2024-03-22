0c5ef84cd4eae387992c561a6546804390806da3
Luis Noreña <luis@criptomart.net>
2024-03-22 17:21:53 +0000

    repopulate __init_.py files removed by pre-commit

---
+++ sports_association/wizards/__init__.py
@@ -0,0 +1,4 @@
+from . import sport_create_issue
+from . import sport_issue_mark_all_done
+from . import sport_league_create_games
+from . import sport_league_create_calendar
---
+++ sports_association_sale/models/__init__.py
@@ -0,0 +1,2 @@
+from . import sport_ticket
+from . import sale_order
